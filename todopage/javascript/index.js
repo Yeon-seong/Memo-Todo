3540ea858b3b41b88487efc677a8be32e8a44a62
yeon-seong <dd300@naver.com>
2024-07-26 02:09:19 +0900

    (추가) HTML 요소를 JS로 가져오기 todo 리스트 제작을 위한 form, input, ul, button 요소 가져오기

---
+++ todopage/javascript/index.js
@@ -0,0 +1,8 @@
+const URL = "http://localhost:3000/todos";
+
+const $todoForm = document.getElementById("todo_form");
+const $todoInput = document.getElementById("todo_input");
+const $todoUl = document.getElementById("todo_ul");
+const addTodoButton = document.getElementsByClassName("add_todo_button");
+const editTodoButton = document.getElementsByClassName("edit_todo_button");
+const deleteodoButton = document.getElementsByClassName("delete_todo_button");
